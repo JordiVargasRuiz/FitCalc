6b26486efb36cfa175728d4d337b207c5aba6599
JordiVargasRuiz <jordi.vargas3669@alumnos.udg.mx>
2025-04-30 22:39:29 -0600

    Primer commit de mi proyecto FitCalc

---
+++ rollup.config.js
@@ -0,0 +1,65 @@
+import { spawn } from 'child_process';
+import svelte from 'rollup-plugin-svelte';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import livereload from 'rollup-plugin-livereload';
+import css from 'rollup-plugin-css-only';
+
+const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		}
+	};
+}
+
+export default {
+	input: 'src/main.js',
+	output: {
+	  sourcemap: true,
+	  format: 'iife',
+	  name: 'app',
+	  file: 'public/build/bundle.js',
+	  // Configuración para GitHub Pages
+	  // Esto le dice a Svelte que los recursos se sirven desde una subcarpeta
+	  base: '/FitCalc/', // Asegúrate de que este valor coincide con tu nombre de repositorio en GitHub
+	},
+	plugins: [
+	  svelte({
+		compilerOptions: {
+		  dev: !production
+		}
+	  }),
+	  css({ output: 'bundle.css' }),
+	  resolve({
+		browser: true,
+		dedupe: ['svelte'],
+		exportConditions: ['svelte']
+	  }),
+	  commonjs(),
+	  !production && serve(),
+	  !production && livereload('public'),
+	  production && terser()
+	],
+	watch: {
+	  clearScreen: false
+	}
+  };
+  
+
---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+  import Navbar from './components/Navbar.svelte';
+  import Footer from './components/Footer.svelte';
+  import ImcCalculator from './components/ImcCalculator.svelte';
+  import Background from './components/Background.svelte';
+</script>
+
+<main>
+  <Background imageUrl="/bg.png" />
+  
+  <Navbar />
+  <div class="welcome-container">
+    <h1><i class="fas fa-star"></i> Bienvenido a FitCalc</h1>
+  </div>
+  <ImcCalculator class="imc-calculator" />
+  <Footer />
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-family: 'Roboto', sans-serif;
+    position: relative;
+  }
+
+  .welcome-container {
+    text-align: center;
+    margin-bottom: 1px;
+	margin-top: 1px;
+  }
+
+  h1 {
+    font-size: 2rem;
+    color: #333;
+    margin-bottom: 5px;
+    font-weight: bold;
+  }
+
+  i {
+    margin-right: 10px;
+    font-size: 3rem;
+    color: #FFD700;
+  }
+
+  .imc-calculator {
+    margin-top: 1px;
+  }
+</style>
